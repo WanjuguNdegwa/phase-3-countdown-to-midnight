6b89803edc29e7e4e99dc7a7d50d5df00fce7671
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-06-11 12:22:55 +0300

    commit tree 2f1d9999383c7a1fab4280bbf8fba0d51b6cabf3

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(interger)
+  while interger >= 1
+    puts "#{interger} SECOND(S)!"
+    interger -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(interger)
+  while interger >= 1
+    puts "#{interger} SECOND(S)!"
+    interger -= 1
+    sleep(1)
+  end
 end
